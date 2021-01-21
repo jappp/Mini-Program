a3e143ffd2b58c209c5da360b605399de49ad824
pengzhaorui <jappp951@163.com>
2021-01-21 14:32:48 +0800

    style: 代码格式化

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,25 +1,26 @@
 const app = getApp();
 var that = null;
 Page({
-    data:{
-      goods:[],
-      loading:true
-    },
-    onLoad(){
-      that = this;
-      // TODO 加载商品列表
-      app.getGoodSList({
-        success:function(list){ //当获取到数据后，执行
-          that.setData({
-            goods:list,
-            loading:false
-          })
-        }
-      })
-    },
-    todetail(e){
-      wx.navigateTo({
-        url: `../detail/detail?id=${e.currentTarget.dataset.id}&name=test`,
-      })
-    }
-})
+  data: {
+    goods: [],
+    loading: true,
+  },
+  onLoad() {
+    that = this;
+    // TODO 加载商品列表
+    app.getGoodSList({
+      success: function (list) {
+        //当获取到数据后，执行
+        that.setData({
+          goods: list,
+          loading: false,
+        });
+      },
+    });
+  },
+  todetail(e) {
+    wx.navigateTo({
+      url: `../detail/detail?id=${e.currentTarget.dataset.id}&name=test`,
+    });
+  },
+});
